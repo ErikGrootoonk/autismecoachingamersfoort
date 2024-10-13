eb72b5176a9c5b26a11515e0845d84980b8e1a40
erik <erik.grootoonk@gmail.com>
2024-10-13 14:35:58 +0200

    invulling pagina's

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.{html,js}',
+   './src/*.js'],
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
